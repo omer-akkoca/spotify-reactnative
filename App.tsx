7c6ed9f44fced9c8192ecaef0cc9337a1ed91bf0
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-07 10:35:40 +0300

    making navigation bar transparent

--- App.tsx
+++ App.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider } from "./src/providers";
 import RootNavigation from "./src/navigation";
+import SystemNavigationBar from "react-native-system-navigation-bar";
 
 const App = () => {
+
+    useEffect(() => {
+        SystemNavigationBar.setNavigationColor('translucent');
+    }, [])
+
     return(
         <SafeAreaProvider>
             <ThemeProvider>
