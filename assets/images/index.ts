a9ae2ba997b1fc414f9812ddf2aeaae764d5d757
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-07 19:54:08 +0300

    added new assets for sing up or sign in screen

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -1,5 +1,6 @@
 import spotifyLogo from "./spotify_logo.png";
 import getStartedBg from "./get_started_bg.png";
 import chooseModeBg from "./choose_mode_bg.png";
+import authBg from "./auth_bg.png";
 
-export { spotifyLogo, getStartedBg, chooseModeBg }
+export { spotifyLogo, getStartedBg, chooseModeBg, authBg }
