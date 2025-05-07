5b8f0e7c4a1140e1ac062443e89f988bcfb26b33
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-07 19:59:08 +0300

    status bar made compatible with theme

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import AppStack from "./app";
+import { StatusBar } from "react-native";
+import { useTheme } from "../providers";
 
 const RootNavigation = () => {
+
+    const { theme } = useTheme()
+
     return(
         <NavigationContainer>
+            <StatusBar barStyle={theme === "dark" ? "light-content" : "dark-content"}/>
             <AppStack/>
         </NavigationContainer>
     )
