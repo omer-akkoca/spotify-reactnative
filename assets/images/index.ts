e7743da9d52b7f8f63cd5e33fe282f0f9ff71f94
omer-akkoca <omerakkoca1042@gmail.com>
2025-05-12 00:07:18 +0300

    home page top card

--- assets/images/index.ts
+++ assets/images/index.ts
@@ -2,5 +2,6 @@ import spotifyLogo from "./spotify_logo.png";
 import getStartedBg from "./get_started_bg.png";
 import chooseModeBg from "./choose_mode_bg.png";
 import authBg from "./auth_bg.png";
+import homeArtist from "./home_artist.png";
 
-export { spotifyLogo, getStartedBg, chooseModeBg, authBg }
+export { spotifyLogo, getStartedBg, chooseModeBg, authBg, homeArtist }
--- src/screens/app/home.tsx
+++ src/screens/app/home.tsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { View } from "react-native";
 import { useTheme } from "../../providers";
-import { ElevatedButton } from "../../components";
-import { signOut } from "../../services";
+import { AppBar, CImage, Space } from "../../components";
+import { HomeTopCard, SearchIcon, VerticalDotsIcon } from "../../../assets/icons";
+import { spotifyLogo, homeArtist } from "../../../assets/images";
+import { width } from "../../constants/responsive";
 
 const HomeScreen = () => {
- 
+
     const { colors } = useTheme()
 
-    return(
+    const homeTopCardHeight = useMemo(() => ((width - 32) * 118) / 334, [])
+
+    return (
         <View style={{ flex: 1, backgroundColor: colors.pageBg }}>
-            <View style={{ flex: 1, justifyContent: "center", alignItems: "center", paddingHorizontal: 16 }}>
-                <ElevatedButton
-                    action={() => signOut()}
-                    title="Sign Out"
-                />
+            <AppBar
+                bgColor="pageBg"
+                center={<CImage local={spotifyLogo} height={35} width={100} mode="contain" />}
+                leading={<SearchIcon color={colors.text} width={25} height={25} />}
+                actions={[
+                    <VerticalDotsIcon color={colors.text} width={25} height={25} />,
+                ]}
+            />
+            <View style={{ flex: 1 }}>
+                <Space height={16} />
+                <View style={{ position: "relative", paddingHorizontal: 16 }}>
+                    <HomeTopCard width={width - 32} height={homeTopCardHeight} />
+                    <CImage
+                        local={homeArtist}
+                        width={width / 2}
+                        height={homeTopCardHeight}
+                        mode="contain"
+                        customStyle={{ position: "absolute", right: 0, bottom: 0 }}
+                    />
+                </View>
             </View>
         </View>
     )
